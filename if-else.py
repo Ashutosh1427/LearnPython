0a2d77ee38efae2dc9cb255b258d9a1cc2ac6597
Ashutosh Kumar Ranjan <ashutosh879731@gmail.com>
2022-04-13 01:54:17 +0530

    new feature added

--- if-else.py
+++ if-else.py
@@ -1,4 +1,13 @@
+# Check even/odd
 if int(input('enter any integer'))%2==0:
     print('Even')
 else:
     print('Odd')
+
+# Check leap/non leap year
+if int(input('enter any year'))%400==0:
+    print('Leap Year')
+elif int(input('enter any year'))%4==0 and int(input('enter any year'))%100!=0:
+    print('Leap Year')
+else:
+    print('Not a Leap Year')
