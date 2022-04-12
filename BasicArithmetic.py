f2dc6cf714b450dc7fa6eb89b01c2e51bf186d6f
Ashutosh Kumar Ranjan <ashutosh879731@gmail.com>
2022-04-13 01:35:37 +0530

    new file added

---
+++ BasicArithmetic.py
@@ -0,0 +1,24 @@
+a=20
+b=10
+print(a+b) #Addition-Both operands can be either number or strings
+print(a-b)
+print(a*b)
+print(a/b) #floating point division
+print(a//b) #floor division-may return as int or float but always floor value of the final result
+print(a%b)
+print(a**b)
+"""
++ 
+    -> It works as arithemetic operator when both the operands are numbers but when both the operands are strings, it will behave as concat operator
+    -> It throws TypeError when one operand is number and other is string
+"""
+print('10'+'20')
+#print('10'+20) #TypeError
+
+"""
+*
+    -> It works as multiplication operator when both the operands are numbers but when one is string and other is integer then it beahves as string repeatation operator
+    -> It throws TypeError when both operands are string
+"""
+print('hello'*2)
+#print('hello'*'world') #TypeError
