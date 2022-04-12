d421776ee118fddc5e780c930ef641400e3a418f
Ashutosh Kumar Ranjan <ashutosh879731@gmail.com>
2022-04-13 02:18:19 +0530

    updated feature

--- Pattern.py
+++ Pattern.py
@@ -2,3 +2,8 @@ for i in range(3):
     for j in range(3):
         print('$',end=' ')
     print(end='\n')
+##############################
+for i in range(3):
+    for j in range(3-i):
+        print('$',end=' ')
+    print(end='\n')
